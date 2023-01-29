1c06884c0c395cc4ceec0fedfee41d3688091614
mubeenasif01 <35574940+mubeenasif01@users.noreply.github.com>
2023-01-29 20:31:55 +0500

    -- Added Some Styling

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.appConrtainer}>
-      <View>
-        <TextInput placeholder='Your course goal!'/>
+      <View style={styles.inputContainer}>
+        <TextInput 
+        style={styles.textInput}
+        placeholder='Your course goal!'/>
         <Button title='Add Goal'/>
       </View>
-      <View>
+      <View style={styles.goalContainer}>
         <Text>Goal List here</Text>
       </View>
     </View>
@@ -17,6 +19,26 @@ export default function App() {
 const styles = StyleSheet.create({
   appConrtainer: {
     paddingTop: 50,
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
+    flex: 1
+  },
+  inputContainer:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: '#cccccc'
+  },
+  textInput:{
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
+  },
+  goalContainer:{
+    flex: 5
   }
 });
